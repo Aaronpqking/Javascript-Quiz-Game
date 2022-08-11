e931772e722281d25a9bae9b0f14040479957042
aKingsView <106622740+aKingsView@users.noreply.github.com>
2022-08-11 01:56:30 -0400

    questions now render properly, code is functional but has a click event malfunction upon second render. started endGame function

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 var startButton = document.querySelector(".start-button");
 
 startButton.addEventListener("click", function (event) {
-    timerStart(); {
-        renderquestion(0);
-    }
-});
+    
+  console.log("timer Started")
+  timerStart();
+  renderquestion();
+  startButton.disabled = true;
+    })
 
 var timer = document.querySelector('.timer');
 var answerBox = document.querySelector('.answerContainer');
 var endMessage = document.querySelector('#scroll-text');
 var timerCount = 90;
 var questionCount = 0;
+var correct = false;
+var score = 0;
 var quiz = [
   {
     question: "Javascript is a programing language",
@@ -26,63 +30,102 @@ var quiz = [
     question: "Which of these are not a Javascript data type?",
     rightanswer: "container",
     answerchoices: ["boolean", "array", "string", "number", "container" ]
-  }
+    }
+    // {
+    //     question: "",
+    //     rightanswer: "",
+    //     answerchoices: "",
+    // },
+    // {
+    //     question: "",
+    //     rightanswer:"",
+    //     answerchoices: "",       
+    // },
+    // {
+    //     question: "",
+    //     rightanswer: "",
+    //     answerchoices: "",
+    // },
+    // {
+    //     question: "",
+    //     rightanswer: "",
+    //     answerchoices: "",
+    // } 
 ]
+
 function timerStart() {
-    // Sets interval in variable
+  
     var timerInterval = setInterval(function() {
       timerCount--;
       timer.innerHTML = timerCount;
   
-      if(timerCount === 0) {
-        // Stops execution of action at set interval
-        clearInterval(timerInterval);
-        // Calls function to create and append image
-        sendMessage();
+      if(timerCount <= 0) {
+       
+          clearInterval(timerInterval);
       }
   
     }, 1000);
-  }
-    
+}
+  
+var questionprompt = document.querySelector(".questionBox");    
+
+  
+var answerlist = document.createElement("ul");
 
 
 function renderquestion() {
-    // Create element:
-    var questionprompt = document.querySelector(".questionBox");
-    questionprompt.innerHTML = quiz[questionCount].question;
-
-    // Append to body:
-    // document.body.appendChild(questionprompt);
-
-    var answerlist = document.createElement("ol");
-    // answerlist.setAttribute("id", "answerlist");
-    answerBox.appendChild(answerlist);
-    // var list = document.getElementById("question");
+  questionprompt.innerHTML = quiz[questionCount].question;
+  answerlist.setAttribute("id", "answerlist");
+  answerBox.appendChild(answerlist);
   
-    quiz[questionCount].answerchoices.forEach((item) => {
-        var li = document.createElement("li");
-        li.innerText = item;
-        answerlist.appendChild(li);
-    })
-    // listen for response
-    answerlist.addEventListener("click", function (event) {
-        if (quiz[questionCount].rightanswer == event.target.innerText) {
-            alert("correct!");
-            questionCount++;
-            if (questionCount > quiz.length || timerCount <= 0) { }
+  for (var i = 0; i < quiz[questionCount].answerchoices.length; i++) {
+    var li = document.createElement("li");
+    li.innerText = quiz[questionCount].answerchoices[i];
+    answerlist.appendChild(li);
+  }
+ 
+  answerlist.addEventListener("click", function (event) {
+    if (quiz[questionCount].rightanswer == event.target.innerText) {
+      alert("CORRECT!");
+      correct = true;
+      console.log("clicked")
+    } else {
+      alert("Incorrect");
+      correct = false;
+    }
+    nextQuestion();
+  })
+}    
+ 
+function nextQuestion() {
 
-        } else {
-            alert("GAME OVER!");
-        } else {
-            alert("Incorrect!");
-            questionCount++;
-            timerCount -= 3;
-            if (questionCount > quiz.length || timerCount <= 0) {
+  while (questionprompt.firstChild) {
+    questionprompt.removeChild(questionprompt.firstChild);
+  }
+  while (answerlist.firstChild) {
+    answerlist.removeChild(answerlist.firstChild);
+  }
+      
+  if (correct == true) {
+    score = score + 100;
+  } else {
+    timerCount -= 3;
+  }
+  questionCount++;
+  if (questionCount < quiz.length && timerCount > 0) {
 
-            } else {
 
-                renderquestion();
-            }
-        }
-})
-} 
+    
+    renderquestion();
+    
+  } else {
+    endGame();
+  }
+}
+
+function endGame() {
+
+  alert("Good Game!!")
+//this function displays score/prompts for initials/displays it added to highscore
+
+}
