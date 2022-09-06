dec51c23ae1f28ecf4dfde485a295bc9eb4b1191
aKingsView <106622740+aKingsView@users.noreply.github.com>
2022-09-06 19:06:35 -0400

    repaired endgame and HS

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 var startButton = document.querySelector("#start-button");
-
+var hsButton = document.querySelector("#hsButton");
+hsButton.addEventListener("click", displayHighscore);
 startButton.addEventListener("click", function (event) {
   console.log("timer Started");
   timerStart();
@@ -9,11 +10,10 @@ startButton.addEventListener("click", function (event) {
 
 var timer = document.querySelector(".timer");
 var answerBox = document.querySelector(".answerContainer");
-var endMessage = document.querySelector("#scroll-text");
-var timerCount = 5;
+var infoBar = document.querySelector("#scroll-text");
 var questionCount = 0;
 var correct = false;
-var score = 0;
+var scores = 0;
 var hsarray = [];
 var timerCount = 90;
 var questionCount = 0;
@@ -87,30 +87,42 @@ function timerStart() {
 
 var questionprompt = document.querySelector(".question-box");
 var quizComplete = document.querySelector(".quiz-complete-screen");
-var answerlist = document.createElement("ul");
+var answerlist = document.createElement("div");
+var highScoreScreen = document.querySelector("#highscore-screen");
+
 
 function renderquestion() {
+  hsButton.disabled = true;
+  questionprompt.style.display = "block";
+  highScoreScreen.style.display = "none";
+  quizComplete.style.display = "none";
   questionprompt.innerHTML = quiz[questionCount].question;
   answerBox.appendChild(answerlist);
+  answerlist.className= "list-group";
   answerlist.setAttribute("id", "answerlist");
+
  
-  answerlist.className= "list-group-item list-group-item-action";
-
-
 
   for (var i = 0; i < quiz[questionCount].answerchoices.length; i++) {
-    var li = document.createElement("li");
+    var li = document.createElement("a");
     li.innerText = quiz[questionCount].answerchoices[i];
+    li.className = "list-group-item list-group-item-action ";
+    li.style.textAlign = "center";
+    li.style.backgroundColor = "#FF7700";
+    li.style.fontSize = "x-large";
+    // li.style.fontWeight = "bold";
     answerlist.appendChild(li);
   }
 
   answerlist.addEventListener("click", function mouseClick(event) {
     if (quiz[questionCount].rightanswer == event.target.innerText) {
-      alert("CORRECT!");
+      infoBar.innerText = "CORRECT!";
+      infoBar.style.backgroundColor = "green";
       correct = true;
       console.log("clicked");
     } else {
-      alert("Incorrect");
+      infoBar.innerText = "INCORRECT";
+      infoBar.style.backgroundColor = "red";
       correct = false;
     }
     this.removeEventListener("click", mouseClick);
@@ -126,6 +138,7 @@ function nextQuestion() {
   if (correct == true) {
     score = score + 100;
   } else {
+    score= score - 25;
     timerCount -= 3;
   }
   // questionCount += 1;
@@ -139,18 +152,14 @@ function nextQuestion() {
 //bug: endGame doesn't end game if last question is left unanswered until after button click.
 function endGame() {
   timerCount = 0;
+  timer.innerHTML = timerCount;
   questionprompt.style.display = "none";
+  highScoreScreen.style.display = "none";
   quizComplete.style.display = "block";
 
 
-  document.getElementById("dis-score").innerHTML = "Your score: " + score;
-
-  score.innerText = score;
-    answerlist.appendChild(li);
-  
-  var input = document.getElementById("score");
-  localStorage.setItem("server", input.val());
-  //this function displays score/prompts for initials/displays it added to highscore
+  infoBar.style.backgroundColor = "white";
+  infoBar.innerHTML = "You scored " + score + " points!";
 
 }
 function saveHighScore() {
@@ -173,8 +182,8 @@ function saveHighScore() {
   bubbleSort(hsarray);
   console.log(bubbleSort);
   localStorage.setItem("highscore", JSON.stringify(hsarray));
-
-  displayHighscore();
+  location.reload();
+//  displayHighscore();
 }
 // Bryan's sort function
 // var sortLesson = [12,342,534,5,1000];
@@ -195,15 +204,22 @@ function bubbleSort(arr) {
 
 function displayHighscore() {
   startButton.disabled = false;
-
-  quizComplete.style.display = "none";
-
-  var highScoreScreen = document.getElementById("highscore-screen");
-
+  questionprompt.style.display = "none";
   highScoreScreen.style.display = "block";
-  var li = document.createElement("li");
-  li.innerText = hsarray[0].name;
-  document.getElementById("scores").appendChild(li);
+  quizComplete.style.display = "block";
+  // scores.className = "list-group-flush";
+  hsarray = JSON.parse(localStorage.getItem("highscore"));
+
+  for (var i = 0; i < hsarray.length; i++) {
+    var li = document.createElement("a");
+    li.innerText = i+1 + ". " + hsarray[i].name + " " + " " + hsarray[i].score;
+    li.style.textAlign = "center";
+    li.style.backgroundColor = "white";
+    li.style.fontSize = "x-large";
+    li.className = "list-group-item";
+    document.getElementById("scores").appendChild(li);
+
+  }
 }
 
 // Bryan's sort function
